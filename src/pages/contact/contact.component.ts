a548792b6b036187744a2b8d646d2f1c5607f061
Igor Pereira <skillbcc@gmail.com>
2025-12-23 16:34:32 +0000

    Adicionado envio de email através do formulário de contacto

--- src/pages/contact/contact.component.ts
+++ src/pages/contact/contact.component.ts
@@ -1,16 +1,23 @@
 
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  private http = inject(HttpClient);
+  
+  isSubmitting = false;
+  submitSuccess = false;
+  submitError = false;
+
   contactForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -20,10 +27,22 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
-      alert('Obrigado pelo seu contacto. A equipa Pilar Forte entrará em contacto brevemente.');
-      this.contactForm.reset();
-    } else {
-      alert('Por favor, preencha todos os campos obrigatórios.');
+      this.isSubmitting = true;
+      this.submitError = false;
+
+      const formspreeUrl = 'https://formspree.io/f/xkowyjrd';
+
+      this.http.post(formspreeUrl, this.contactForm.value).subscribe({
+        next: () => {
+          this.isSubmitting = false;
+          this.submitSuccess = true;
+          this.contactForm.reset();
+        },
+        error: () => {
+          this.isSubmitting = false;
+          this.submitError = true;
+        }
+      });
     }
   }
 }
